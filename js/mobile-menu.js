e04a33bbf325c58b145ef2f514ada2852cb8d71e
OleksiiVTS <119164270+OleksiiVTS@users.noreply.github.com>
2023-01-22 22:36:49 +0200

    +

--- js/mobile-menu.js
+++ js/mobile-menu.js
@@ -3,11 +3,12 @@
   const openMenuBtn = document.querySelector(".js-open-menu");
   const closeMenuBtn = document.querySelector(".js-close-menu");
 
+
   const toggleMenu = () => {
     const isMenuOpen =
       openMenuBtn.getAttribute("aria-expanded") === "true" || false;
-    openMenuBtn.setAttribute("aria-expanded", !isMenuOpen);
-    mobileMenu.classList.toggle("is-open");
+      openMenuBtn.setAttribute("aria-expanded", !isMenuOpen);
+      mobileMenu.classList.toggle("is-open");
 
     const scrollLockMethod = !isMenuOpen
       ? "disableBodyScroll"
